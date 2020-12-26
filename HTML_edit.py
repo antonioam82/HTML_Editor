7ac2247a79271d70b71d675159023b3ef4f6b90f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-12-26 17:28:52 +0100

    Update HTML_edit.py

--- HTML_edit.py
+++ HTML_edit.py
@@ -1,13 +1,14 @@
 import sys
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import (QWidget, QApplication, QLineEdit,
-QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit, QMainWindow, QAction)
+QVBoxLayout, QHBoxLayout, QPushButton, QMessageBox, QTextEdit, QMainWindow, QFileDialog, QAction)
 
 class HTMLeditor(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(800, 750)
-        self.setWindowTitle('HTML Editor') 
+        self.setWindowTitle('HTML Editor')
+        self.file_path = None
 
         self.nav_bar = QHBoxLayout()
 
@@ -31,6 +32,7 @@ class HTMLeditor(QWidget):
         self.btnTry = QPushButton("PROBAR")
         self.btnTry.clicked.connect(self.get_code)
         self.btnSave = QPushButton("GUARDAR")
+        self.btnSave.clicked.connect(self.save_doc)
         root.addWidget(self.web_view)
         root.addWidget(self.textEdit)
         root.addWidget(self.btnTry)
@@ -40,11 +42,37 @@ class HTMLeditor(QWidget):
         self.setLayout(root)
 
     def get_code(self):
-        mytext = self.textEdit.toPlainText()
-        if mytext != "":
-            self.html=mytext
+        self.mytext = self.textEdit.toPlainText()
+        if self.mytext != "":
+            self.html=self.mytext
             self.web_view.setHtml(self.html)
             #print(mytext)
+
+    def save_doc(self):
+        if not self.file_path:
+            new_file_path, filter_type = QFileDialog.getSaveFileName(self, "Save this file as...", "", "HTML Files(*.html)")
+            if new_file_path:
+                self.file_path = new_file_path
+                print("ok1")
+            else:
+                self.invalid_path_alert_message()
+                return False
+        file_contents = self.textEdit.toPlainText()
+        print("ok2")
+        with open(self.file_path, "w") as f:
+            f.write(file_contents)
+        print("OK3")
+        self.file_path = None
+        #self.title.setText(self.file_path)
+        print("ok4")
+
+    
+    def invalid_path_alert_message(self):
+        messageBox = QMessageBox()
+        messageBox.setWindowTitle("Invalid file")
+        messageBox.setText("Selected filename or path is not valid. Please select a valid file.")
+        messageBox.exec()                
+            
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
